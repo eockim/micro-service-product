3e13616348d82127513bd9e029d3d1f7ce5e6b32
kimhyungjun <hyungjoon6876@gmail.com>
2018-05-24 23:36:29 +0900

    auth2

--- src/main/java/com/micro/product/ProductApplication.java
+++ src/main/java/com/micro/product/ProductApplication.java
@@ -10,16 +10,19 @@ import com.micro.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@EnableEurekaClient
 @SpringBootApplication
 @RestController
-@EnableEurekaClient
+@EnableOAuth2Sso
 public class ProductApplication {
 
 
